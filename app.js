9868b6c74561ab5645530bb98ddcde645fa1228b
luana almeida <luanaa297@gmail.com>
2024-08-20 13:50:43 -0300

    criando a função de sortear

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-function sortear() {
+function sortear(){
+    // pegando os elementos do HTML
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    
     let sorteados = [];
     let numero;
+    
+    // Criando laço para repetir o numero de acordo com a quantidade pedida
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
-        sorteados.push(valor);
+        sorteados.push(numero);
     }
+    // mostrando números sorteados
     alert(sorteados);
 }
+// função que gera números aleatórios
 function obterNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
